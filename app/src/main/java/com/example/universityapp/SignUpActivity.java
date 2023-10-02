01f42f61714f8b6c687c20cf31a42bf089212702
Mukhammadnur <gsjxne@gmail.com>
2023-10-02 15:24:59 +0300

    личный кабинет

--- app/src/main/java/com/example/universityapp/SignUpActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.universityapp;
-
-public class SignUpActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initView()
-    }
-}
